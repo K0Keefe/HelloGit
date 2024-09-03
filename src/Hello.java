d5dc939ff1a8d85455a88b3c1fecd87cb87f42fc
kokee <kokee@10.69.17.119>
2024-09-03 16:02:44 -0500

    Added Name and major print out

--- src/Hello.java
+++ src/Hello.java
@@ -8,7 +8,9 @@
 public class Hello {
 
   public static void main(String args[]) {
-    System.out.println("Hello World!");
+    System.out.println("Hello World!\n");
+    
+    System.out.println("My name is Kevin O'Keefe and I am a Computer Science Major");
   }
 
 }
