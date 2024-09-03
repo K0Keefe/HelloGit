9aa3a4e0f532ec039bc0ff5c11d6613c273b08e1
Ethan <Ethan@Ethan-Laptop.unl.edu>
2024-09-03 16:25:25 -0500

    Added my name and major!

--- src/Hello.java
+++ src/Hello.java
@@ -11,6 +11,9 @@ public class Hello {
     System.out.println("Hello World!\n");
     
     System.out.println("My name is Kevin O'Keefe and I am a Computer Science Major");
+    
+    System.out.println("My name is Ethan Steele!");
+    System.out.println("I am a Computer Science major!");
   }
 
 }
