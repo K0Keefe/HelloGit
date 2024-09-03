32f18040d88ca71457a2f5ceac523297c3e84220
kokee <kokee@10.69.17.119>
2024-09-03 15:41:52 -0500

    Initial Commit

---
+++ src/Hello.java
@@ -0,0 +1,14 @@
+/**
+ * Kevin O'Keefe
+ * 
+ * CSE155H, Hack 1, Java Section
+ * 
+ */
+
+public class Hello {
+
+  public static void main(String args[]) {
+    System.out.println("Hello World!");
+  }
+
+}
